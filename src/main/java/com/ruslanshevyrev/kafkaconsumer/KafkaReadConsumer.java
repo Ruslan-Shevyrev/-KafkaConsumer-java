119a114471c0c026bdf48c2cbad42a82f0d61a81
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-05-30 21:55:15 +0300

    KafkaReadConsumer.java

--- src/main/java/com/ruslanshevyrev/kafkaconsumer/KafkaReadConsumer.java
+++ src/main/java/com/ruslanshevyrev/kafkaconsumer/KafkaReadConsumer.java
@@ -14,7 +14,7 @@ public class KafkaReadConsumer {
             props.setProperty("bootstrap.servers", "localhost:9092");
             props.setProperty("group.id", "test");
             props.setProperty("enable.auto.commit", "true");
-            props.setProperty("auto.commit.interval.ms", "1000");
+            //props.setProperty("auto.commit.interval.ms", "1000");
             props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
             props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
             KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
@@ -24,6 +24,7 @@ public class KafkaReadConsumer {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> record : records)
                     System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                    consumer.commitAsync();
             }
 	}
 }
